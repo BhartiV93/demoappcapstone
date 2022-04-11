14bd7045ee319bc821c7297250cb3e93badabec2
BhartiV93 <87116878+BhartiV93@users.noreply.github.com>
2022-04-11 09:59:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <center>
         <h1>Welcome</h1>
         <h2>To</h2>
-        <h3>Tech Saksham</h3>
-        <h4>Cloud</h4>
-        <h5>Computing</h5>
+        <h3>Cloud Computing</h3>
+        <h4>Tech</h4>
+        <h5>Saksham</h5>
         <h6>web design</h6>
         </center>
         <p>
